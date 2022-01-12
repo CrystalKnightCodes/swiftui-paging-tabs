48e912f91eafccf45c5916ffb610da24e8bd0e51
Crystal Knight <cknight@strava.com>
2022-01-12 16:39:26 -0500

    Create project

---
+++ swiftui-paging-tabs/swiftui-paging-tabs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-paging-tabs
+//
+//  Created by Crystal Knight on 1/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-paging-tabs/swiftui-paging-tabs/swiftui_paging_tabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_paging_tabsApp.swift
+//  swiftui-paging-tabs
+//
+//  Created by Crystal Knight on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_paging_tabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
